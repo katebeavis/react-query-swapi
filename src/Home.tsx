6019662bcb709a1c4b462d6758c0d50f672b07fc
Kate Beavis <katebeavis84@gmail.com>
2020-12-15 19:08:29 +0000

    Add REST useQuery

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,10 +7,27 @@ const Home = () => {
     const json = await res.json();
     return json;
   };
-  const { isLoading, data } = useQuery('todos', fetchData);
+
+  const { isLoading, data, error } = useQuery('restPeopleQuery', fetchData);
 
   console.log({ isLoading, data });
-  return <h1>Hi</h1>;
+  if (error) {
+    console.log(error);
+    return <>error</>;
+  }
+  return (
+    <>
+      <h1>React query swapi</h1>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <h2>REST example</h2>
+          <h3>{data.name}</h3>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
